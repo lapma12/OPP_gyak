8e324c4510c03661840562791b771b802bb34718
Lapma <lapostyanm@kkszki.hu>
2025-09-08 12:12:16 +0200

    Feladat2 is ready to use

--- OPP gyak/Program.cs
+++ OPP gyak/Program.cs
@@ -8,19 +8,27 @@ namespace OPP_gyak
 {
     public class Szemely
     {
-        public string nev;
-        public int eletkor;
+        private string nev;
+        private int eletkor;
 
-        public Szemely(string nev, int eletkor)
+        public Szemely(string Nev, int Eletkor)
         {
-            this.nev = nev;
-            this.eletkor = eletkor;
+            this.nev = Nev;
+            this.eletkor = Eletkor;
         }
-    }
+        public string Kiir()
+        {
+            return $"Személy neve: {nev} és életkora: {eletkor}";
+        }
+    }   
     internal class Program
     {
         static void Main(string[] args)
         {
+            Szemely person = new Szemely("Ilona", 31);
+            Console.WriteLine(person.Kiir());
+
+            Console.ReadKey();
         }
     }
 }
