f300ddf60023643d2868fea82029c471cf8ffb51
Lapma <lapostyanm@kkszki.hu>
2025-09-08 11:43:17 +0200

    The Szemely class is created with properties

--- OPP gyak/Program.cs
+++ OPP gyak/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace OPP_gyak
 {
+    public class Szemely
+    {
+        string nev;
+        int eletkor;
+
+        public Szemely(string nev, int eletkor)
+        {
+            this.nev = nev;
+            this.eletkor = eletkor;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            
         }
     }
 }
